270f269811de6ad571bb8a217700159059222d51
Vice079 <v_llorens@hotmail.com>
2025-02-07 00:53:06 +0100

    intermedia no va

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ entrada.addEventListener("keydown", valoracion)
 
 
 function valoracion() {
-    if (entrada.value.length <8) {
+    if (entrada.value.length < 8) {
         const infodata = document.getElementById("info")
-        infodata.innerText = "Contraseña no segura"
-        infodata.style.color = "red"
-    } else if (entrada.value.lenght =8 && entrada.value.lenght <10) {
-        infodata.innerText = "Contraseña de seguridad intermedia"
-        infodata.style.color = "blue"
-    }  else if (entrada.value.lenght >10) {
+        infodata.innerText = "Contraseña no segura";
+        infodata.style.color = "red";
+    } else if (entrada.value.lenght >= 8 && entrada.value.lenght <= 10) {
+        const infodata = document.getElementById("info")
+        infodata.innerText = "Contraseña de seguridad intermedia";
+        infodata.style.color = "blue";
+    }  else {
+        const infodata = document.getElementById("info")
         infodata.innerText = "Contraseña segura"
         infodata.style.color = "green"
-    }
-    }
+    }   
+}         

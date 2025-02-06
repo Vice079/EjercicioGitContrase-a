ffa2bae71dc04c3ac15a1ed363dc3209db10a77e
Vice079 <v_llorens@hotmail.com>
2025-02-06 21:58:50 +0100

    Creada funcion pero no va del todo bien

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hola")
+const entrada = document.getElementById("entrada")
+entrada.addEventListener("keydown", valoracion)
+
+
+function valoracion() {
+    if (entrada.value.length <8) {
+        const infodata = document.getElementById("info")
+        infodata.innerText = "Contraseña no segura"
+        infodata.style.color = "red"
+    } else if (entrada.value.lenght =8 && entrada.value.lenght <10) {
+        infodata.innerText = "Contraseña de seguridad intermedia"
+        infodata.style.color = "blue"
+    }  else if (entrada.value.lenght >10) {
+        infodata.innerText = "Contraseña segura"
+        infodata.style.color = "green"
+    }
+    }

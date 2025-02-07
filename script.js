21a8fbb2d2ff5dc4ca50549740f161ec23651f30
silviodyankov <silviodqnkov@hotmail.com>
2025-02-07 13:25:57 +0100

    Funcionalidad Login

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-console.log("Hola");
 const entrada = document.getElementById("entrada");
+const boton = document.getElementById("boton");
+const infodata = document.getElementById("info");
+
 entrada.addEventListener("keyup", valoracion);
 
 function valoracion() {
     let longitud = entrada.value.length;
-    const infodata = document.getElementById("info");
     if (longitud < 8) {
         infodata.innerText = "Contraseña no segura";
         infodata.style.color = "red";
+        boton.disabled = true;
     } else if (longitud >= 8 && longitud <= 10) {
         infodata.innerText = "Contraseña de seguridad intermedia";
         infodata.style.color = "blue";
+        boton.disabled = true;
     } else {
         infodata.innerText = "Contraseña segura";
         infodata.style.color = "green";
+        boton.removeAttribute("disabled");
     }
 }

fe0c72d7ab267d46a89641db33ba5c4b37d56435
Vice079 <v_llorens@hotmail.com>
2025-02-07 09:25:53 +0100

    Ahora si funciona bien

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 console.log("Hola");
 const entrada = document.getElementById("entrada");
-entrada.addEventListener("keydown", valoracion);
+entrada.addEventListener("keyup", valoracion);
 
 function valoracion() {
     let longitud = entrada.value.length;
+    const infodata = document.getElementById("info");
     if (longitud < 8) {
-        const infodata = document.getElementById("info");
         infodata.innerText = "Contraseña no segura";
         infodata.style.color = "red";
     } else if (longitud >= 8 && longitud <= 10) {
-        const infodata = document.getElementById("info");
         infodata.innerText = "Contraseña de seguridad intermedia";
         infodata.style.color = "blue";
     } else {
-        const infodata = document.getElementById("info");
         infodata.innerText = "Contraseña segura";
         infodata.style.color = "green";
     }
